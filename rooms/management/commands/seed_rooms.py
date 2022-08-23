2e7aa6d67c710d84575ed0d235e8ece37e1f26d0
ellajang <3lla.jang@gmail.com>
2022-08-23 22:29:16 +0900

    fourth_airbnb

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,7 +1,6 @@
 import random
-from secrets import choice
-from unicodedata import name
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django_seed import Seed
 from rooms import models as room_models
 from users import models as user_models
@@ -39,5 +38,32 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+
+        created_photos = seeder.execute()
+        created_clean = flatten(list(created_photos.values()))
+        amenity = room_models.Amenity.objects.all()
+        facilities = room_models.Facility.objects.all()
+        rules = room_models.HouseRule.objects.all()
+        for pk in created_clean:
+            room = room_models.Room.objects.get(pk=pk)
+            for i in range(3, random.randint(10, 17)):
+                room_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"room_photos/{random.randint(1,31)}.webp",
+                )
+
+            for a in amenity:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.amenity.add(a)
+            for f in facilities:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.facility.add(f)
+            for r in rules:
+                magic_number = random.randint(0, 15)
+                if magic_number % 2 == 0:
+                    room.house_rules.add(r)
+
         self.stdout.write(self.style.SUCCESS(f"{number} user created!"))
